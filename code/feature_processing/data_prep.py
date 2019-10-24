ccfe1368b83b89eeab733ef1960ee1102dd1eb5c
Anastassia Kornilova <annie237@gmail.com>
2019-10-24 15:35:07 -0400

    add cleaning code

--- code/feature_processing/data_prep.py
+++ code/feature_processing/data_prep.py
@@ -1,83 +1,58 @@
 '''
-Just setup dataset
+Modify the dataset to use clean texts
 '''
+import os
 import pandas as pd 
 
-from utilities.resro_helper import ResroHelper
-from utilities.text_preprocessing import TextPreprocessor
+from feature_processing.text_preprocessor import TextPreprocessor
 
-data = pd.read_pickle('../data/us_data/train_20052012_all.pkl')
-data2 = pd.read_pickle('../data/us_data/test_20052012_all.pkl')
-data = pd.concat([data, data2])
+# Change prefix to point to your data directory
+# Make a clean_text directory
+PREFIX = '../data/raw_data'
+CLEAN_PREFIX = '../data/clean_data'
 
-# First fetch all titles and summaries
+def clean_summary(session):
 
-resro = ResroHelper()
+    data = pd.read_pickle(os.path.join(PREFIX, f'full_data_{session}.pkl'))
+    # Fix nulls in some summaries
+    data = data.fillna('')
 
-bills = data.groupby('natural_id').first()
+    text_data = pd.read_pickle(os.path.join(PREFIX, f'bill_text_{session}.pkl'))
 
-new_titles = {}
-new_summaries = {}
-for bid in bills.index:
+    tp = TextPreprocessor()
 
-    locality, session, eid = bid.split('_')
+    final_texts = {}
 
-    query = ''' SELECT title, summary 
-                FROM legislation.bills 
-                WHERE locality='us' and session=:session and external_id=:eid
-            '''
+    for _, row in text_data.iterrows():
 
-    title, summary = resro.session.execute(query, params={'session': session, 'eid': eid}).fetchone()    
+        clean_text = ' '.join(tp.tokenize(row.full_text))
 
-    new_titles[bid] = title 
-    new_summaries[bid] = summary
-
-data['title'] = data.natural_id.map(new_titles)
-data['summary'] = data.natural_id.map(new_summaries)
+        final_texts[row.natural_id] = clean_text
 
 
-# Next let's clean them up
-tp = TextPreprocessor()
+    final_summary = {}
 
-# Summaries first
-summaries = data.groupby('natural_id').first().summary
-text_map = dict(summaries)
-text_map2 = {}
-for k,v in text_map.items():
-    v = v or '' # Sometimes summaries are null
-    text_map2[k] = ' '.join(tp.tokenize(v))
+    for i, row in data.groupby('natural_id').first().iterrows():
 
-data['summary_clean_tp'] = data.natural_id.map(text_map2)
+        title_summary = ' '.join([row.title, row.summary])
 
-# Clean up the title too
-text_fields = data.groupby('natural_id').first().title
-text_map = dict(data.groupby('natural_id').first().title
-)
-text_map2 = {}
+        tokens = list(tp.tokenize(title_summary))
+        clean_text = ' '.join(tokens)
 
-for k,v in text_map.items():
-    text_map2[k] = ' '.join(tp.tokenize(v))
+        # If the resulting text is less than 20 words - attach the bill text
+        if len(tokens) < 20:
+            clean_text += ' ' + final_texts[i]
 
-data['title_clean_tp'] = data.natural_id.map(text_map2)
+        final_summary[i] = clean_text
 
-data['title_summary_clean_tp'] = data['title_clean_tp'] + ' ' + data['summary_clean_tp']
+    data['target_summary'] = data.natural_id.map(final_summary)
 
-# For our final field - use title+summary. If too short, use the text 
+    data['target_text'] = data.natural_id.map(final_texts)
 
-final_fields = {}
-for i, row in data.groupby('natural_id').first().iterrows():
-    
-    title_summary = row.title_clean_tp + row.summary_clean_tp
-    
-    if len(set(title_summary.split())) < 50:
-        # Clean up text first
-        text = ' '.join(tp.tokenize(row.text))
-        final_fields[i] = text
-    else:
-        final_fields[i] = title_summary
-data['special_text'] = data.natural_id.map(final_fields)
+    data.to_pickle(os.path.join(CLEAN_PREFIX, f'full_data_{session}_clean.pkl'))
 
-# For space purposes - only save key fields
-data = data[['natural_id', 'chamber', 'leg_id', 'd_perc', 'r_perc', 'special_text', 'party', 'vote']]
 
-data.to_pickle('../data/us_data/full_20052012_all_summary.pkl')
+if __name__ == '__main__':
+
+    for session in ['20052012', '20132014', '20152016']:
+        clean_summary(session)
